e538ba82dd340492270f2be0392c7a840ba959d9
Jeffrey <jeffk613@gmail.com>
2025-10-02 00:17:30 -0400

    For assignment submission

--- q2.py
+++ q2.py
@@ -7,7 +7,7 @@ import numpy as np
 import cv2
 import tyro
 
-from q1 import load_annotated, affine_rect
+from q1 import load_annotated, affine_rect, add_lines
 import utils
 
 def metric_from_affine(lines):
@@ -34,40 +34,66 @@ def metric_from_affine(lines):
     H_m = np.diag(np.sqrt(1 / E)) @ Uh
     return H_m
 
-def annotate_img(img, lines):
-    return img
     
-def main(data_path: str = "data", output_path: str = "output"):
+def main(data_path: str = "data", output_path: str = "output/q2"):
+    Path(output_path).mkdir(parents=True, exist_ok=True)
     imgs_annos_aff = load_annotated(data_path, "q1")
     imgs_annos_met = load_annotated(data_path, "q2")
     
     for aff_anno, met_anno in zip(imgs_annos_aff, imgs_annos_met):
-        img_name, img, _, lines_aff = astuple(aff_anno)
+        img_name, img, points_aff, lines_aff = astuple(aff_anno)
+        *_, points_met, lines_met = astuple(met_anno)
         H_a = affine_rect(lines_aff)
         affine_img = utils.MyWarp(img, H_a)
         H_a_inv = np.linalg.inv(H_a)
         # H_a_inv /= H_a_inv[2,2]
 
         affine_lines = lines_aff @ H_a_inv
+        points_met_affine = points_met @ H_a.T
         cv2.imwrite(
             str(
-                (Path(output_path) / f"{img_name}_affine").with_suffix(".jpg")
+                (Path(output_path) / f"{img_name}_train_unrectified_affine").with_suffix(".jpg")
             ),
-            annotate_img(affine_img, affine_lines)
+            add_lines(img, points_met[:8], seed=10)
+        )
+        cv2.imwrite(
+            str(
+                (Path(output_path) / f"{img_name}_train_rectified_affine").with_suffix(".jpg")
+            ),
+            add_lines(affine_img, points_met_affine[:8], seed=10)
+        )
+        cv2.imwrite(
+            str(
+                (Path(output_path) / f"{img_name}_eval_unrectified_affine").with_suffix(".jpg")
+            ),
+            add_lines(img, points_met[8:], seed=20)
+        )
+        cv2.imwrite(
+            str(
+                (Path(output_path) / f"{img_name}_eval_rectified_affine").with_suffix(".jpg")
+            ),
+            add_lines(affine_img, points_met_affine[8:], seed=20)
         )
         affine_angles = (utils.cosine(*affine_lines[4:6, :]), utils.cosine(*affine_lines[6:8, :]))
 
-        *_, lines_met = astuple(met_anno)
         H_m = metric_from_affine(lines_met)
         H_m_inv = np.linalg.inv(H_m)
         # H_m_inv /= H_m_inv[2,2]
         metric_img = utils.MyWarp(img, H_m @ H_a) # wonder what would happen if I did MyWarp(img, H_m @ H_a)
         metric_lines = lines_met @ (H_m_inv)
+        points_met_metric = points_met @ H_m_inv.T
+        # pdb.set_trace()
         cv2.imwrite(
             str(
-                    (Path(output_path) / f"{img_name}_metric").with_suffix(".jpg")
+                    (Path(output_path) / f"{img_name}_train_rectified_metric").with_suffix(".jpg")
             ),
-            annotate_img(metric_img, metric_lines)
+            add_lines(metric_img, points_met_metric[:8], seed=10)
+        )
+        cv2.imwrite(
+            str(
+                    (Path(output_path) / f"{img_name}_eval_rectified_metric").with_suffix(".jpg")
+            ),
+            add_lines(metric_img, points_met_metric[8:], seed=20)
         )
         metric_angles = (utils.cosine(*metric_lines[4:6, :]), utils.cosine(*metric_lines[6:8, :]))
 
